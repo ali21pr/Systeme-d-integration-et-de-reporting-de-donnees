902ae8845ab23a4863f7d8c7c82b4138c0ddde0e
ali21pr <116568003+ali21pr@users.noreply.github.com>
2023-08-31 17:11:05 +0200

    Delete Code/Use case data Interagtion and reporting directory

--- Code/Use case data Interagtion and reporting/First_Project_BP/Infra_As_Code/service_account.tf
+++
@@ -1,9 +0,0 @@
-/*
-* Service Account
-*/
-resource "google_service_account" "service_acc" {
-  for_each     = var.service_accs
-  account_id   = "sac-${lower(each.value.purpose)}"
-  display_name = each.value.display_name
-  description  = each.value.description
-}
